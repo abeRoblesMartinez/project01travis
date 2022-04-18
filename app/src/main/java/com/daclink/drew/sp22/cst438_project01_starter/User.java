5aba64e4b6244fbd52046603d994add03eba511a
Abraham David Robles Martinez <98565007+abeRoblesMartinez@users.noreply.github.com>
2022-04-17 22:28:00 -0700

    getters and setters

--- app/src/main/java/com/daclink/drew/sp22/cst438_project01_starter/User.java
+++ app/src/main/java/com/daclink/drew/sp22/cst438_project01_starter/User.java
@@ -3,4 +3,20 @@ package com.daclink.drew.sp22.cst438_project01_starter;
 public class User {
     String name;
     String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
